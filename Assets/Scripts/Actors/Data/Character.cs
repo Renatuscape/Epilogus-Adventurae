d747f02aef2044d7c28dec1b4ab9c19e56a18d1f
Renatuscape <rrjonsberg@gmail.com>
2025-06-14 20:57:30 +0200

    Move character class to actor folder

---
+++ Assets/Scripts/Actors/Data/Character.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Character
+{
+    public (int x, int y) targetPoi;
+    public (int x, int y) coor = (0, 2);
+    public GameObject corpus;
+    public List<Behaviour> behaviours = new();
+
+    // NPC movement and point of interest
+
+
+    public void Move(ref (int x, int y) poi, (int x, int y) player)
+    {
+
+    }
+}
