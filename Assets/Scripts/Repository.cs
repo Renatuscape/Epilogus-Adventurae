761891ef97ea2be20136baed3fc679051e79e28d
Renatuscape <rrjonsberg@gmail.com>
2025-06-14 21:00:54 +0200

    Add repository class

---
+++ Assets/Scripts/Repository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class Repository
+{
+    public static List<Blueprint> Districts { get; set; } = new();
+    public static List<Blueprint> Buildings { get; set; } = new();
+
+    public static Blueprint FindBlueprintById(string id, BlueprintType type)
+    {
+        if (type == BlueprintType.Building)
+        {
+            return Buildings.FirstOrDefault(x => x.Id == id);
+        }
+        else
+        {
+            return Districts.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
